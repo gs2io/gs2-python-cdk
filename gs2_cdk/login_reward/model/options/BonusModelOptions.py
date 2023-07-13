94ad94579eb56e6df07a5493bdfbe1f7bf5474e4
gs2-ci <ci@gs2.io>
2023-07-13 09:54:37 +0000

    4fcba74977a05016967ea200bfdb556b8f49df78

---
+++ gs2_cdk/login_reward/model/options/BonusModelOptions.py
@@ -0,0 +1,47 @@
+# Copyright 2016- Game Server Services, Inc. or its affiliates. All Rights
+# Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+from __future__ import annotations
+from typing import *
+from ....core.model import AcquireAction
+from ..Reward import Reward
+from ....core.model import ConsumeAction
+from ..enum.BonusModelMode import BonusModelMode
+from ..enum.BonusModelRepeat import BonusModelRepeat
+from ..enum.BonusModelMissedReceiveRelief import BonusModelMissedReceiveRelief
+
+
+class BonusModelOptions:
+    metadata: Optional[str]
+    period_event_id: Optional[str]
+    reset_hour: Optional[int]
+    repeat: Optional[BonusModelRepeat]
+    rewards: Optional[List[Reward]]
+    missed_receive_relief_consume_actions: Optional[List[ConsumeAction]]
+    
+    def __init__(
+        self,
+        metadata: Optional[str] = None,
+        period_event_id: Optional[str] = None,
+        reset_hour: Optional[int] = None,
+        repeat: Optional[BonusModelRepeat] = None,
+        rewards: Optional[List[Reward]] = None,
+        missed_receive_relief_consume_actions: Optional[List[ConsumeAction]] = None,
+    ):
+        self.metadata = metadata
+        self.period_event_id = period_event_id
+        self.reset_hour = reset_hour
+        self.repeat = repeat
+        self.rewards = rewards
+        self.missed_receive_relief_consume_actions = missed_receive_relief_consume_actions
+
