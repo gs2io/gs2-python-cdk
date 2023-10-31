11c3fb98374f30fe46cb629efc80bf74bdf183ef
gs2-ci <ci@gs2.io>
2023-10-31 13:45:37 +0000

    78f5204dd420f7b7e7743df95cce10b432247656

--- gs2_cdk/script/model/options/RandomStatusOptions.py
+++ gs2_cdk/script/model/options/RandomStatusOptions.py
@@ -11,11 +11,17 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
-from .Namespace import Namespace
-from .options.NamespaceOptions import NamespaceOptions
-from .Script import Script
-from .options.ScriptOptions import ScriptOptions
-from .RandomStatus import RandomStatus
-from .options.RandomStatusOptions import RandomStatusOptions
-from .RandomUsed import RandomUsed
-from .options.RandomUsedOptions import RandomUsedOptions
+from __future__ import annotations
+from typing import *
+from ..RandomUsed import RandomUsed
+
+
+class RandomStatusOptions:
+    used: Optional[List[RandomUsed]]
+    
+    def __init__(
+        self,
+        used: Optional[List[RandomUsed]] = None,
+    ):
+        self.used = used
+
