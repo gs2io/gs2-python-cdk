8aff556dabbe595b7ce48fb37695d476c9ee64a7
gs2-ci <ci@gs2.io>
2026-09-24 12:56:14 +0000

    85fd6126a4e330c3e3644853d71d0390f9f77a74

--- gs2_cdk/distributor/model/__init__.py
+++ gs2_cdk/distributor/model/__init__.py
@@ -20,8 +20,6 @@ from .options.BatchRequestPayloadOptions import BatchRequestPayloadOptions
 from .enums.BatchRequestPayloadService import BatchRequestPayloadService
 from .BatchResultPayload import BatchResultPayload
 from .options.BatchResultPayloadOptions import BatchResultPayloadOptions
-from .UserDataEntry import UserDataEntry
-from .options.UserDataEntryOptions import UserDataEntryOptions
 from .AcquireActionResult import AcquireActionResult
 from .options.AcquireActionResultOptions import AcquireActionResultOptions
 from .ConsumeActionResult import ConsumeActionResult
